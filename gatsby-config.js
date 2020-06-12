2f2380d1ffa06f573c6f0f60b882bdef1a34f175
Vipul Chodankar <vipul72c@gmail.com>
2020-06-12 23:47:46 +0530

    Initial Setup with gatsby-cli

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `FOSS Community GEC`,
+    description: `The Beta Website of The Foss Community GEC. FOSS community GEC is a club started by a group of FOSS enthusiasts to nurture the open source community in Goa College of Engineering and encourage students across Goa to contribute to open source software projects and adopt the FOSS philosophy.`,
+    author: `FCGEC`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `fcgec-webapp`,
+        short_name: `fcgec-webapp`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
