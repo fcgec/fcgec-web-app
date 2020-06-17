e9b2d19fb4678417226717a883b4f6932dbb6879
Neil Botelho <neil.botelho321@gmail.com>
2020-06-17 20:22:04 +0530

    Add descriptions in index.js
    Replaced the placeholder text in the index page for the "who are we" and
    "what do we do" sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,16 +45,18 @@ const IndexPage = () => {
         </div>
         <div className={indexStyles.text}>
           <h2>Who we are</h2>
-          <p>FOSS community GEC is a club started by a group of FOSS enthusiasts to nurture the open source community in Goa College of Engineering and encourage students across Goa to contribute to open source software projects and adopt the FOSS philosophy.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod quibusdam consectetur earum nobis vitae odit maiores repellat. Aliquam autem veritatis reiciendis aspernatur fuga repellat? Assumenda, maxime. Pariatur modi saepe blanditiis!</p>
+          <br>
+          <p>We're a bunch of people who love free and open source software. We created this community to connect with other FOSS enthusiasts both in our college and beyond. Everyone is welcome at our events from 1st year college students to working professionals.</p>
         </div>
       </section>
 
       <section className={indexStyles.imageWithText}>
         <div className={indexStyles.text}>
           <h2>What we do</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quasi nulla commodi autem sit aliquam, rem ducimus dolor cumque quas voluptatem, beatae nihil corporis deleniti facilis harum nisi voluptatum quidem.</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolorem veritatis assumenda culpa quaerat perspiciatis neque pariatur ea exercitationem ipsam, tenetur reiciendis enim voluptate debitis deserunt voluptatibus aliquid aperiam quasi.</p>
+          <br>
+          <br>
+          <p>We strive to nurture the open source community in our college and encourage students across Goa to contribute to open source software projects and adopt the FOSS.</p>
+          <p>We also regularly organise workshops and talks on various Open source technologies and projects. So keep an eye on our Events page to for upcoming programs.</p>
         </div>
         <div className={indexStyles.image}>
           <WhatWeDo />
