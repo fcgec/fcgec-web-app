8482d6bff6a445dfcd10869b4f37d640855a31b3
Vipul Chodankar <vipul72c@gmail.com>
2020-06-19 19:36:36 +0530

    Updated Event Registration Form

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 
 import axios from 'axios'
+import cx from 'classnames'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -11,9 +12,12 @@ const RegisterPage = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [messages, setMessages] = useState();
+    const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState();
 
     const submitHandler = event => {
         event.preventDefault();
+        setLoading(true);
         axios({
             method: 'post',
             url: 'https://fcgec-events.herokuapp.com/api/register',
@@ -22,9 +26,18 @@ const RegisterPage = () => {
                 email
             }
         })
-            .then(response => { console.log(response); setMessages(response.data.message) })
-            .catch(error => console.error(error));
-
+            .then(response => {
+                // console.log(response);
+                setMessages(response.data.message);
+                setLoading(false);
+                setSuccess(true);
+            })
+            .catch(error => {
+                console.error(error);
+                setMessages('Something went wrong');
+                setLoading(false);
+                setSuccess(false);
+            });
     }
 
     return (
@@ -36,34 +49,46 @@ const RegisterPage = () => {
             </section>
 
             <div className="container">
-                <form onSubmit={submitHandler} className={registerStyles.form}>
-                    {messages ? <div>{messages}</div> : ""}
-                    <div>
-                        <p>Enter your name</p>
-                        <input type="text"
-                            onChange={e => setName(e.target.value)}
-                            value={name}
-                            required
-                            aria-label="input to take name"
-                        // minLength={2}
-                        />
-                    </div>
-                    <div>
-                        <p>Enter your Email</p>
-                        <input type="email"
-                            onChange={e => setEmail(e.target.value)}
-                            value={email}
-                            required
-                            aria-label="input to take email"
-                        // minLength={2}
-                        />
-                    </div>
-                    <div>
-                        <input type="Submit"
-                            aria-label="Button to submit form"
-                        />
-                    </div>
-                </form>
+                <div className={registerStyles.container}>
+                    <p className={registerStyles.privacy}>
+                        Your data is completely safe with us.
+                    </p>
+                    {messages ?
+                        <span className={cx(registerStyles.message, success ? 'success' : 'failed')}>
+                            {`${messages}.`} <br />
+                            {(success ? 'We hope to see you soon :)' : 'Please try again :(')}
+                        </span>
+                        : ""}
+
+                    <form onSubmit={submitHandler} className={registerStyles.form}>
+
+                        <div>
+                            <p>Name</p>
+                            <input type="text"
+                                onChange={e => setName(e.target.value)}
+                                value={name}
+                                required
+                                aria-label="input to take name"
+                            />
+                        </div>
+                        <div>
+                            <p>Email</p>
+                            <input type="email"
+                                onChange={e => setEmail(e.target.value)}
+                                value={email}
+                                required
+                                aria-label="input to take email"
+                            />
+                        </div>
+                        <div>
+                            <input type="Submit"
+                                disabled={loading}
+                                aria-label="Button to submit form"
+                            />
+                        </div>
+                    </form>
+
+                </div>
             </div>
 
         </Layout >
